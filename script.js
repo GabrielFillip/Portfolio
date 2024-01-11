8a1cd5e0c3ae3267a13a6f41078e5b9c03d73e85
Gabriel Fillip <142547884+GabrielFillip@users.noreply.github.com>
2024-01-11 19:32:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function LinkCalc(){
+window.location = "https://github.com/GabrielFillip/Calculadora";
+}
+
+function LinkCasavenus(){
+    window.location = "https://github.com/GabrielFillip/Casa_venus";
+}
+
+function LinkBD(){
+    window.location = "https://github.com/GabrielFillip/Banco-de-dados-Appolo-Rotas";
+}
+
+function LinkApollo(){
+    window.location = "https://github.com/habbiner/apollo-rotas";
+}
